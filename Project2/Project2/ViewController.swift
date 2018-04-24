4de153e69f303878fc367844bc81fe92d44d9148
HYUNGJIN KIM <kimhy@10-150-44-23.dhcp-m.deakin.edu.au>
2018-04-24 17:19:16 +1000

    check in

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by HYUNGJIN KIM on 24/4/18.
+//  Copyright © 2018 HYUNGJIN KIM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
